c2e1db43bf5cf7d3a1f73e0189d13db7d5b6e5ea
Dan Hermann <dan.hermann@elastic.co>
2019-01-17 13:58:26 -0600

    Update output plugin for beta version of API

--- src/main/java/org/logstash/javaapi/JavaOutputExample.java
+++ src/main/java/org/logstash/javaapi/JavaOutputExample.java
@@ -2,11 +2,10 @@ package org.logstash.javaapi;
 
 import co.elastic.logstash.api.Configuration;
 import co.elastic.logstash.api.Context;
+import co.elastic.logstash.api.Event;
 import co.elastic.logstash.api.LogstashPlugin;
+import co.elastic.logstash.api.Output;
 import co.elastic.logstash.api.PluginConfigSpec;
-import co.elastic.logstash.api.v0.Output;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import org.logstash.Event;
 
 import java.io.OutputStream;
 import java.io.PrintStream;
@@ -16,38 +15,36 @@ import java.util.Iterator;
 import java.util.concurrent.CountDownLatch;
 
 // class name must match plugin name
-@LogstashPlugin(name="java_output_example")
+@LogstashPlugin(name = "java_output_example")
 public class JavaOutputExample implements Output {
 
     public static final PluginConfigSpec<String> PREFIX_CONFIG =
-            Configuration.stringSetting("prefix", "");
+            PluginConfigSpec.stringSetting("prefix", "");
 
+    private final String id;
     private String prefix;
     private PrintStream printer;
     private final CountDownLatch done = new CountDownLatch(1);
     private volatile boolean stopped = false;
 
-    // all plugins must provide a constructor that accepts Configuration and Context
-    public JavaOutputExample(final Configuration configuration, final Context context) {
-        this(configuration, context, System.out);
+    // all plugins must provide a constructor that accepts id, Configuration, and Context
+    public JavaOutputExample(final String id, final Configuration configuration, final Context context) {
+        this(id, configuration, context, System.out);
     }
 
-    JavaOutputExample(final Configuration config, final Context context, OutputStream targetStream) {
+    JavaOutputExample(final String id, final Configuration config, final Context context, OutputStream targetStream) {
         // constructors should validate configuration options
+        this.id = id;
         prefix = config.get(PREFIX_CONFIG);
         printer = new PrintStream(targetStream);
     }
 
     @Override
     public void output(final Collection<Event> events) {
-        try {
-            Iterator<Event> z = events.iterator();
-            while (z.hasNext() && !stopped) {
-                String s = prefix + z.next().toJson();
-                printer.println(s);
-            }
-        } catch (JsonProcessingException e) {
-            throw new IllegalStateException(e);
+        Iterator<Event> z = events.iterator();
+        while (z.hasNext() && !stopped) {
+            String s = prefix + z.next();
+            printer.println(s);
         }
     }
 
@@ -66,5 +63,10 @@ public class JavaOutputExample implements Output {
     public Collection<PluginConfigSpec<?>> configSchema() {
         // should return a list of all configuration options for this plugin
         return Collections.singletonList(PREFIX_CONFIG);
+    }
+
+    @Override
+    public String getId() {
+        return id;
     }
 }
